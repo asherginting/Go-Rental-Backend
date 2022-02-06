8e7ce6f1d3b3f6baa6ff85f8ba9cafbcd5c07b1f
Asher Azriel Ginting <asherginting3@gmail.com>
2022-02-06 22:45:12 +0700

    fix: controllers profile

--- src/controllers/profile.js
+++ src/controllers/profile.js
@@ -1,20 +1,35 @@
-const modelProfile = require('../models/profile');
+const prof = require('../models/profile');
 
-const getProfile = (req, res) => {
-    const { id } = req.params;
-    modelProfile(id, (results) => {
-        if (results.length > 0) {
-            return res.json({
-                success: true,
-                message: `Detail profile with id ${id}`,
-                results: results[0],
-            });
-        }
-        return res.status(404).json({
-            success: false,
-            message: `User with id ${id} not found`,
+const getProfile = (req, res)=>{
+    const {id} = req.params;
+    if(id>0){
+        prof.getProfile(id, result=>{
+            if(result.length>0){
+                return res.json({
+                    success: true,
+                    message: `User profile with ID: ${id}`,
+                    result: result
+                });
+            }else{
+                return res.status(404).send({
+                    success: false,
+                    message: `User with ID: ${id} not found`
+                });
+            }
         });
-    });
+    }else{
+        return res.status(400).send({
+            success: false,
+            message: 'ID should be a number greater than 0'
+        });
+    }
+  
+    if(id==undefined || id==null){
+        return res.status(400).send({
+            success: false,
+            message: 'Undefined ID'
+        });
+    }
 };
 
-module.exports = getProfile;
+module.exports = {getProfile};
