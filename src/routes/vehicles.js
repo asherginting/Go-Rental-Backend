c79eaaa82760dcd7492b6cbe0dbae830be0ca121
Asher Azriel Ginting <asherginting3@gmail.com>
2022-02-06 21:51:23 +0700

    feat: add getCategory and addVehicle

--- src/routes/vehicles.js
+++ src/routes/vehicles.js
@@ -3,18 +3,22 @@ const vehicles = require('express').Router();
 const {
     getVehicles,
     getVehicle,
-    addNewVehicle,
+    addVehicle,
     updateVehicle,
     deleteVehicle,
-    getVehicleCategory
+    getCategory
+
 } = require('../controllers/vehicles');
 
 
 vehicles.get('/', getVehicles);
-vehicles.get('/category', getVehicleCategory);
+vehicles.get('/category/', getCategory);
+vehicles.get('/category/:category_id', getCategory);
 vehicles.get('/:id', getVehicle);
-vehicles.post('/', addNewVehicle);
-vehicles.put('/:id', updateVehicle);
+vehicles.post('/', addVehicle);
+vehicles.patch('/', updateVehicle);
+vehicles.patch('/:id', updateVehicle);
+vehicles.delete('/', deleteVehicle);
 vehicles.delete('/:id', deleteVehicle);
 
 
