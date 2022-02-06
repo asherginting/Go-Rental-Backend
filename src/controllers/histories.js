d1f2140bc9862bcd5e41836c502ddc680717b648
Asher Azriel Ginting <asherginting3@gmail.com>
2022-02-06 22:47:48 +0700

    fix: controllers histories

--- src/controllers/histories.js
+++ src/controllers/histories.js
@@ -1,198 +1,225 @@
-const usersModel = require('../models/users');
-const {
-    dataValidator,
-    requestReceiver,
-    validateId
-} = require('../helpers/requestHandler');
-const {
-    returningError,
-    returningSuccess
-} = require('../helpers/responseHandler');
-const historiesModel = require('../models/histories');
+const rentHistories = require('../models/histories');
+const vehicles = require('../models/vehicles');
+const users = require('../models/users');
 
-
-const listHistories = async (req, res) => {
-    try {
-        const keys = [
-            'id', 'user_id', 'vehicle_id', 'payment_code', 'payment', 'returned',
-            'prepayment'
-        ];
-
-        const results = await historiesModel.getHistories(keys);
-
-        return returningSuccess(res, 200, 'Success getting histories', results);
-    } catch (error) {
-        console.error(error);
-        return returningError(res, 500, 'Failed to get list of history');
-    }
-};
-
-const addHistory = async (req, res) => {
-    try {
-        const keys = [
-            'user_id',
-            'vehicle_id',
-            'payment_code',
-            'payment',
-            'returned',
-            'prepayment'
-        ];
-        const data = requestReceiver(req.body, keys);
-
-        // validate inputed data
-        const isValidate = dataValidator(data);
-
-        if (!isValidate) {
-            return res.status(400).json({
-                success: true,
-                message: 'Data not validate'
-            });
-        }
-
-        // check if user exist
-        const result = await usersModel.getUser(data.user_id);
-
-        if (result.length < 1) {
-            return res.status(404).json({
-                success: false,
-                message: 'User not found'
-            });
-        }
-
-        // check if history has beed added
-        const historyResult = await historiesModel.addHistory(data);
-
-        if (historyResult.affectedRows < 1) {
-            return res.status(400).json({
-                success: false,
-                message: 'Cant make transaction'
-            });
-        }
-
-        return res.status(201).json({
+const getHistories = (req, res)=>{
+    let {vehicle_name, page, limit} = req.query;
+    vehicle_name = vehicle_name || '';
+    page = parseInt(page) || 1;
+    limit = parseInt(limit) || 5;
+    const offset = (page-1)*limit;
+    const data = {vehicle_name, page, limit, offset};
+    rentHistories.getHistories(data, results=>{
+        return res.json({
             success: true,
-            message: 'Transaction is created'
+            message: 'Histories',
+            result: results
         });
-    } catch (error) {
-        console.log(error);
+    });
+};
+
+const getHistory = (req, res)=>{
+    const {id} = req.params;
+    rentHistories.getHistory(id, results=>{
+        if(results.length>0){
+            return res.json({
+                success: true,
+                message: `Rent history with ID: ${id}`,
+                result: results[0]
+            });
+        }else{
+            return res.status(404).send({
+                success: false,
+                message: `ID: ${id} not found`
+            });
+        }
+    });
+};
+
+const getHistoryId = (req, res)=>{
+    const {vehicle_id} = req.params;
+    let {page, limit} = req.query;
+    page = parseInt(page) || 1;
+    limit = parseInt(limit) || 5;
+    const offset = (page-1)*limit;
+    const data = {page, limit, offset, vehicle_id};
+    if(vehicle_id!==null || vehicle_id!==undefined){
+        if(vehicle_id>0){
+            rentHistories.getHistoryId(data, result=>{
+                if(result.length>0){
+                    return res.json({
+                        success: true,
+                        message: `History rent with vehicle_id=${vehicle_id}`,
+                        result: result
+                    });
+                }else{
+                    return res.status(404).send({
+                        success: false,
+                        message: 'Vehicle not found'
+                    });
+                }
+            });
+        }else{
+            return res.status(400).send({
+                success: false,
+                message: 'vehicle_id should be a number greater than 0.'
+            });
+        }
+    }else{
+        return res.status(400).send({
+            success: false,
+            message: 'Undefined ID'
+        });
     }
 };
 
-const deleteHistory = async (req, res) => {
-    try {
-        const {
-            id
-        } = req.params;
-
-        // validate inputed id
-        if (!validateId(id)) {
-            return returningError(res, 400, 'Id must be a number');
+const isNull = (data)=>{
+    let a = 0;
+    for(let i=0; i<data.length;i++){
+        if(data[i]==''){
+            a++;
         }
+    }
+    return a;
+};
 
-        // check if history exist
-        const history = await historiesModel.getHistory(id);
-
-        // if history exist
-        if (history.length > 0) {
-            // delete history
-            const result = await historiesModel.deleteHistory(id);
-
-            // if history can't to delete
-            if (result.affectedRows < 1) {
-                return returningError(res, 400, 'cant delete history');
+const dataType = (data)=>{
+    const dataName = ['vehicle_id', 'user_id', 'prepayment', 'rent_date', 'return_date'];
+    const dataThrow = [];
+    const dataError = [];
+    for(let i=0; i<dataName.length;i++){
+        if(i<3){
+            dataThrow.push(parseInt(data[i]));
+        }else{
+            dataThrow.push(data[i].toString());
+        }
+    }
+    for(let j=0; j<dataName.length;j++){
+        if(j<3 && isNaN(dataThrow[j])==true){
+            dataError.push(`${dataName[j]} should be a number`);
+        }else if(j>=3){
+            if(data[j].length<10){
+                dataError.push(`${dataName[j]} is invalid date.`);
             }
-
-            // if history deleted
-            return returningSuccess(res, 200, 'History has been deleted', history);
+            else{
+                let a = new Date(data[j]);
+                if(a=='Invalid Date'){
+                    dataError.push(`${dataName[j]} is invalid date.`);
+                }
+            }
         }
+    }
+    return dataError;
+};
 
-        // if history not exist
-        return returningError(res, 404, 'History not found');
-    } catch (error) {
-        // if error exist
-        console.log(error);
-        return returningError(res, 500, 'Failed to delete history');
+const addHistories = (req, res)=>{
+    const {vehicle_id, user_id, prepayment, rent_date, return_date} = req.body;
+    const data = [vehicle_id, user_id, prepayment, rent_date, return_date];
+    let b = isNull(data);
+    let c = dataType(data);
+    const cb = (resAdd)=>{
+        rentHistories.getHistVId(vehicle_id, resGetId=>{
+            rentHistories.editHistory(vehicle_id, ress=>{
+                if(resAdd.affectedRows>0 && resGetId.length>0 && ress.affectedRows>0){
+                    return res.json({
+                        success: true,
+                        message: 'Successfully add histories.',
+                        result : resGetId[resGetId.length-1]
+                    });
+                }else{
+                    return res.status(500).send({
+                        success: false,
+                        message: 'Server error.'
+                    });
+                }
+            });
+        });
+    };
+    if(b>0){
+        return res.status(400).send({
+            success: false,
+            message: 'Please fill in all the fields.'
+        });
+    }
+    if(c.length>0){
+        return res.status(400).send({
+            success: false,
+            message: c
+        });
+    }
+    vehicles.getVehicle(vehicle_id, result=>{
+        if(result.length>0){
+            if(result[0].available>0){
+                users.getUser(user_id, resUser=>{
+                    if(resUser.length>0){
+                        rentHistories.addHistories(data, cb);
+                    }else{
+                        return res.status(404).send({
+                            success: false,
+                            message: `User with ID: ${user_id} not found`
+                        });
+                    }
+                });
+            }else{
+                return res.status(400).send({
+                    success: false,
+                    message: 'Vehicle not available at the time.'
+                });
+            }
+        }else{
+            return res.status(404).send({
+                success: false,
+                message: `Vehicle with ID: ${vehicle_id} not found.`
+            });
+        }
+    });
+};
+
+const updateHistory = (req, res)=>{
+    const {id} = req.params;
+    const {vehicle_id, user_id, prepayment, rent_date, return_date} = req.body;
+    const data = [vehicle_id, user_id, prepayment, rent_date, return_date, id];
+    let b = isNull(data);
+    let c = dataType(data);
+    if(b>0){
+        return res.status(400).send({
+            success: false,
+            message: 'Please fill in all the fields.'
+        });
+    }
+    if(c.length>0){
+        return res.status(400).send({
+            success: false,
+            message: c
+        });
     }
 };
 
-const upadateHistory = async (req, res) => {
-    try {
-        const {
-            id
-        } = req.params;
-        const keys = [
-            'payment', 'returned', 'prepayment'
-        ];
-        const data = requestReceiver(req.body, keys);
-
-        // validate inputed id
-        if (!validateId(id)) {
-            return returningError(res, 400, 'Id must be a number');
-        }
-
-        // validate inputed data
-        const isValidate = dataValidator(data);
-
-        if (!isValidate) {
-            return returningError(res, 400, 'Data not validate');
-        }
-
-        // check if history exist
-        const history = await historiesModel.getHistory(id);
-
-        // if history not exist
-        if (history.length < 1) {
-            return returningError(res, 404, 'History not found');
-        }
-
-        // update history
-        const result = await historiesModel.updateHistory(id, data);
-
-        // if history can't to update
-        if (result.affectedRows < 1) {
-            return returningError(res, 500, 'Cant update history');
-        }
-
-        // get updated history
-        const updatedHistory = await historiesModel.getHistory(id);
-
-        return returningSuccess(res, 200, 'History has been updated', updatedHistory[0]);
-    } catch (error) {
-        console.log(error);
-        console.error(error);
-        return returningError(res, 500, 'Failed to update history');
+const deleteHistory = (req, res)=>{
+    const {id} = req.params;
+    if(id!==null && id!==undefined){
+        rentHistories.getHistory(id, results=>{
+            if(results.length>0){
+                rentHistories.deleteHistory(id, result=>{
+                    return res.json({
+                        success: true,
+                        message: `History with ID: ${id} was deleted`,
+                        result: `Rows affected: ${result.affectedRows}`
+                    });
+                });
+            }else{
+                return res.status(404).send({
+                    success: false,
+                    message: `ID: ${id} not found`
+                });
+            }
+        });
+    }else{
+        return res.status(400).send({
+            success: false,
+            message: 'Undefined ID'
+        });
     }
 };
 
-const getHistory = async (req, res) => {
-    try {
-        const {
-            id
-        } = req.params;
-
-        // validate inputed id
-        if (!validateId(id)) {
-            return returningError(res, 400, 'Id must be a number');
-        }
-
-        const result = await historiesModel.getHistory(id);
-
-        // if history not exist
-        if (result.length < 1) {
-            return returningError(res, 404, 'History not found');
-        }
-
-        return returningSuccess(res, 200, 'Success getting history', result[0]);
-    } catch (error) {
-        console.error(error);
-        return returningError(res, 500, 'Failed to get history');
-    }
-};
-
-module.exports = {
-    listHistories,
-    addHistory,
-    deleteHistory,
-    upadateHistory,
-    getHistory
-};
+module.exports = {getHistories, getHistory, getHistoryId, addHistories, updateHistory, deleteHistory};
