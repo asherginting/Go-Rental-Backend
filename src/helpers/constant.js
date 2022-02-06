fcde43a9a04213385ec631ea62572c427f996209
Asher Azriel Ginting <asherginting3@gmail.com>
2022-02-06 22:37:17 +0700

    chore: delete file in helpers

--- src/helpers/constant.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    usersTable: 'users',
-    historiesTable: 'histories',
-    vehiclesTable: 'vehicles',
-    categoriesTable: 'categories',
-    baseURL: 'http://localhost:5000'
-};
