e6a56221288b4f7aa57b7534eb7a702d2657f9cb
Asher Azriel Ginting <asherginting3@gmail.com>
2022-02-06 22:20:01 +0700

    fix: models users

--- src/models/users.js
+++ src/models/users.js
@@ -1,101 +1,65 @@
 const db = require('../helpers/db');
 const table = 'users';
 
-exports.getUsers = (limit, offset, search) => {
-    const name = search.name || '';
-    const email = search.email || '';
-
-    return new Promise((resolve, reject) => {
-        db.query(`SELECT id, name, email, gender, address, birthdate, phone FROM ${table} WHERE name LIKE '${name}%' AND email LIKE '${email}%' LIMIT ? OFFSET ?`, [limit, offset], (err, res) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(res);
-            }
-        });
+exports.getUsers = (data, cb)=>{
+    db.query(`SELECT * FROM ${table} WHERE name LIKE '%${data.name}%' LIMIT ${data.limit} OFFSET ${data.offset}`, (err,res)=>{
+        if(err) throw err;
+        cb(res);
     });
 };
 
-exports.getUser = (id) => {
-    return new Promise((resolve, reject) => {
-        db.query(`SELECT * FROM ${table} WHERE id = ?`, [id], (err, res) => {
-            if (err) {
-                reject(err);
-            }
-            resolve(res);
-        });
+exports.getGender = (data, cb)=>{
+    db.query(`SELECT * FROM ${table} WHERE name LIKE '%${data.name}%' AND gender=${data.gender} LIMIT ${data.limit} OFFSET ${data.offset}`, (err,res)=>{
+        if(err) throw err;
+        cb(res);
     });
 };
 
-exports.insertUser = (data) => {
-    return new Promise((resolve, reject) => {
-        const query = `INSERT INTO ${table} SET ?`;
-        db.query(query, data, (err, res) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(res);
-            }
-        });
+exports.getUser = (id, cb)=>{
+    db.query(`SELECT * FROM ${table} WHERE id =?`, [id], (err, res)=>{
+        if(err) throw err;
+        cb(res);
     });
 };
 
-exports.updateUser = (id, data) => {
-    return new Promise((resolve, reject) => {
-        db.query(`UPDATE ${table} SET ? WHERE id = ?`, [data, id], (err, res) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(res);
-            }
-        });
+exports.checkContact = (contact, cb)=>{
+    db.query(`SELECT * FROM ${table} WHERE email=? OR phone_number=?`, contact, (err, res)=>{
+        if(err) throw err;
+        cb(res);
     });
 };
 
-exports.deleteUser = (id) => {
-    return new Promise((resolve, reject) => {
-        db.query(`DELETE FROM ${table} WHERE id = ? `, [id], (err, res) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(res);
-            }
-        });
+exports.checkEmail = (email, cb)=>{
+    db.query(`SELECT * FROM ${table} WHERE email=?`, [email], (err, res)=>{
+        if(err) throw err;
+        cb(res);
     });
 };
 
-exports.countUsers = () => {
-    return new Promise((resolve, reject) => {
-        db.query(`SELECT COUNT(*) AS 'rows' FROM ${table}`, (err, res) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(res);
-            }
-        });
+exports.checkPhone = (phone, cb)=>{
+    db.query(`SELECT * FROM ${table} WHERE phone_number=?`, [phone], (err, res)=>{
+        if(err) throw err;
+        cb(res);
     });
 };
 
-exports.findEmail = (email) => {
-    return new Promise((resolve, reject) => {
-        db.query(`SELECT COUNT(*) AS 'rows' FROM ${table} WHERE email = ?`, [email], (err, res) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(res);
-            }
-        });
+exports.addUser = (data, cb)=>{
+    db.query(`INSERT INTO ${table} (name, email, password, phone_number, gender, birthdate, address) VALUES(?,?,?,?,?,?,?)`, data, (err, res)=>{
+        if(err) throw err;
+        cb(res);
     });
 };
 
-exports.findPhone = (phone) => {
-    return new Promise((resolve, reject) => {
-        db.query(`SELECT COUNT(*) AS 'rows' FROM ${table} WHERE phone = ?`, [phone], (err, res) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(res);
-            }
-        });
+exports.updateUser = (data, cb)=>{
+    db.query(`UPDATE ${table} SET name=?, email=?, password=?, phone_number=?, gender=?, birthdate=?, address=? WHERE id=?`, data, (err,res)=>{
+        if(err) throw err;
+        cb(res);
+    });
+};
+
+exports.deleteUser = (id, cb)=>{
+    db.query(`DELETE FROM ${table} WHERE id=?`, [id], (err,res)=>{
+        if(err) throw err;
+        cb(res);
     });
 };
