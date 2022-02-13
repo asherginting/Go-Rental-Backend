ecff6e09f660cadbac218a08a129cfa069e772ee
Asher Azriel Ginting <asherginting3@gmail.com>
2022-02-13 22:29:42 +0700

    feat: add auth route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,11 @@
 const users = require('express').Router();
 
-const { login, ForgotPass } = require('../controllers/auth');
+const { login, register, forgotPass, logout, verify } = require('../controllers/auth');
 
 users.post('/login', login);
-users.post('/forgot-password', ForgotPass);
+users.post('/register', register);
+users.post('/forgotPass', forgotPass);
+users.post('/logout', logout);
+users.post('/verify', verify);
 
 module.exports = users;
