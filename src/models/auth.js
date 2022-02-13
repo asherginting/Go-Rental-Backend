4bd7d3bee5573cc71dd28f4288bfdc37a9b4eb53
Asher Azriel Ginting <asherginting3@gmail.com>
2022-02-13 22:30:15 +0700

    feat: add model auth

---
+++ src/models/auth.js
@@ -0,0 +1,45 @@
+const db = require('../helpers/db');
+
+exports.login = (data)=>{
+    return new Promise((resolve, reject)=>{
+        db.query('SELECT * FROM users WHERE username=?', [data.username], (err, res)=>{
+            if(err) reject(err);
+            resolve(res);
+        });
+    });
+};
+
+exports.getUsername = (username) => new Promise((resolve, reject)=>{
+    db.query(`SELECT id, username, email, password, role FROM users WHERE username='${username}' OR email='${username}'`, (err, res)=>{
+        if(err) reject(err);
+        resolve(res);
+    });
+});
+
+exports.requestPassword = (data)=>new Promise((resolve, reject)=>{
+    db.query(`INSERT INTO forgot_request (user_id, code, expired_date) VALUES(${data.user_id}, ${data.code}, ${data.expired_date})`, (err, res)=>{
+        if(err) reject(err);
+        resolve(res);
+    });
+});
+
+exports.updateReqPassword = (id)=>new Promise((resolve, reject)=>{
+    db.query('UPDATE forgot_request SET status = 0 WHERE id=?', [id], (err, res)=>{
+        if(err) reject(err);
+        resolve(res);
+    });
+});
+
+exports.updatePassword = (password, id)=>new Promise((resolve, reject)=>{
+    db.query('UPDATE users SET password=? WHERE id=?', [password, id], (err, res)=>{
+        if(err) reject(err);
+        resolve(res);
+    });
+});
+
+exports.codeCompare = (code)=>new Promise((resolve, reject)=>{
+    db.query(`SELECT * FROM forgot_request WHERE code=${code}`, (err, res)=>{
+        if(err) reject(err);
+        resolve(res);
+    });
+});
