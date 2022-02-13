68524f5127cc2e092260ca7903d5ab5cc880b157
Asher Azriel Ginting <asherginting3@gmail.com>
2022-02-13 21:36:56 +0700

    fix: db query profile

--- src/models/profile.js
+++ src/models/profile.js
@@ -2,7 +2,7 @@ const db = require('../helpers/db');
 const table = 'users';
 
 exports.getProfile = (id, cb)=>{
-    db.query(`SELECT name, email, phone_number, gender, birthdate, address FROM ${table} WHERE id=?`, [id], (err, res)=>{
+    db.query(`SELECT name, email, username, password, phone_number, gender, birthdate, address FROM ${table} WHERE id=?`, [id], (err, res)=>{
         if(err) throw err;
         cb(res);
     });
