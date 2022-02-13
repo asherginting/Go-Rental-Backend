ca208b27fbbc81efcbc4851937259d3919466a69
Asher Azriel Ginting <asherginting3@gmail.com>
2022-02-13 20:53:41 +0700

    feat: add auth route

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const route = require('express').Router();
 
 route.use('/vehicles', require('./vehicles'));
 route.use('/users', require('./users'));
+route.use('/auth', require('./auth'));
 route.use('/histories', require('./histories'));
 route.use('/profile', require('./profile'));
 route.use('/popular', require('./popular'));
