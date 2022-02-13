0b762281f693f60ca785469622fbc6eeb26fd69a
Asher Azriel Ginting <asherginting3@gmail.com>
2022-02-13 21:20:03 +0700

    feat: add asynchronous

--- src/models/vehicles.js
+++ src/models/vehicles.js
@@ -8,6 +8,13 @@ exports.getVehicles = (data, cb)=>{
     });
 };
 
+exports.getVehicles = (data) => new Promise ((resolve, reject)=>{
+    db.query(`SELECT * FROM ${table} WHERE name LIKE '%${data.name}%' AND location LIKE '%${data.location}%' AND cost>=${data.cost_min} AND cost<=${data.cost_max} LIMIT ${data.limit} OFFSET ${data.offset}`, (err, res)=>{
+        if(err) reject(err);
+        resolve(res);
+    });
+});
+
 exports.getVehicle = (id, cb)=>{
     db.query(`SELECT * FROM ${table} WHERE id=?`,[id], (err,res)=>{
         if (err) throw err;
@@ -15,12 +22,12 @@ exports.getVehicle = (id, cb)=>{
     });
 };
 
-exports.getCategory = (data, cb)=>{
-    db.query(`SELECT * FROM ${table} WHERE name LIKE '%${data.name}%' AND location LIKE '%${data.location}%' AND cost>=${data.cost_min} AND cost<=${data.cost_max} AND category_id=${data.category_id} LIMIT ${data.limit} OFFSET ${data.offset}`, (err,res)=>{
-        if (err) throw err;
-        cb(res);
+exports.getVehicle = (id) => new Promise ((resolve, reject)=>{
+    db.query(`SELECT * FROM ${table} WHERE id=${id}`, (err, res)=>{
+        if(err) reject(err);
+        resolve(res);
     });
-};
+});
 
 exports.addVehicle = (data, cb)=>{
     db.query(`INSERT INTO ${table} (name, image, year, cost, available, type, seat, category_id, location) VALUES (?,?,?,?,?,?,?,?,?)`, [data.name, data.image, data.year, data.cost, data.available, data.type, data.seat, data.category_id, data.location], (err, res) => {
@@ -28,6 +35,13 @@ exports.addVehicle = (data, cb)=>{
         cb(res);
     });  
 };
+
+exports.addVehicle = (data)=> new Promise ((resolve, reject)=>{
+    db.query(`INSERT INTO ${table} (name, image, year, cost, available, type, seat, category_id, location) VALUES('${data.name}', ${data.image}, '${data.year}', ${data.cost}, ${data.available}, '${data.type}', ${data.seat}, ${data.category_id}, '${data.location}')`, (err, res)=>{
+        if(err) reject(err);
+        resolve(res);
+    });
+});
 
 exports.checkVehicle = (isThere, cb)=>{
     db.query(`SELECT * FROM ${table} WHERE name=?`,[isThere], (err,res)=>{
@@ -43,12 +57,33 @@ exports.updateVehicle = (data, cb)=>{
     });
 };
 
+exports.updateVehicle = (data, id)=>new Promise((resolve, reject)=>{
+    db.query(`UPDATE ${table} SET ? WHERE id=?`, [data, id], (err, res)=>{
+        if(err) reject(err);
+        resolve(res);
+    });
+});
+
 exports.deleteVehicle = (id, cb)=>{
     db.query(`DELETE FROM ${table} WHERE id = ?`, [id],
         (err, res)=>{
             if (err) throw err;
             cb(res);
         });
+};
+
+exports.deleteVehicle = (id)=>new Promise((resolve, reject)=>{
+    db.query(`DELETE FROM ${table} WHERE id=?`, [id], (err, res)=>{
+        if(err) reject(err);
+        resolve(res);
+    });
+});
+
+exports.getCategory = (data, cb)=>{
+    db.query(`SELECT * FROM ${table} WHERE name LIKE '%${data.name}%' AND location LIKE '%${data.location}%' AND cost>=${data.cost_min} AND cost<=${data.cost_max} AND category_id=${data.category_id} LIMIT ${data.limit} OFFSET ${data.offset}`, (err,res)=>{
+        if (err) throw err;
+        cb(res);
+    });
 };
 
 exports.showVehicle = (data, cb)=>{
@@ -58,9 +93,16 @@ exports.showVehicle = (data, cb)=>{
     });
 };
 
-exports.countVehicles = (data, cb) => {
-    db.query(`SELECT COUNT(*) as total FROM ${table} WHERE name LIKE '${data.search}%'`, (err, res) => {
-        if (err) throw err;
-        cb(res);
+exports.countData = (data)=>new Promise((resolve, reject)=>{
+    db.query(`SELECT COUNT(*) AS total FROM vehicles WHERE name LIKE '%${data.name}%' AND location LIKE '%${data.location}%' AND cost>=${data.cost_min} AND cost<=${data.cost_max}`, (err, res)=>{
+        if(err) reject(err);
+        resolve(res);
     });
-};
+});
+
+exports.getVehicleName = (data)=>new Promise((resolve, reject)=>{
+    db.query(`SELECT * FROM vehicles WHERE name='${data.name}' && year='${data.year}' && cost=${data.cost} && location='${data.location}'`, (err, res)=>{
+        if(err) reject(err);
+        resolve(res);
+    });
+});
