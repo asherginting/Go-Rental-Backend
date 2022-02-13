56eb291e613f689448ea209aa83bd744dd68aff4
Asher Azriel Ginting <asherginting3@gmail.com>
2022-02-13 21:33:03 +0700

    feat: change to asynchronous

--- src/models/users.js
+++ src/models/users.js
@@ -1,19 +1,12 @@
 const db = require('../helpers/db');
 const table = 'users';
 
-exports.getUsers = (data, cb)=>{
-    db.query(`SELECT * FROM ${table} WHERE name LIKE '%${data.name}%' LIMIT ${data.limit} OFFSET ${data.offset}`, (err,res)=>{
-        if(err) throw err;
-        cb(res);
+exports.getUsers = ()=> new Promise((resolve, reject)=>{
+    db.query(`SELECT * FROM ${table}`, (err, res)=>{
+        if(err) reject(err);
+        resolve(res);
     });
-};
-
-exports.getGender = (data, cb)=>{
-    db.query(`SELECT * FROM ${table} WHERE name LIKE '%${data.name}%' AND gender=${data.gender} LIMIT ${data.limit} OFFSET ${data.offset}`, (err,res)=>{
-        if(err) throw err;
-        cb(res);
-    });
-};
+});
 
 exports.getUser = (id, cb)=>{
     db.query(`SELECT * FROM ${table} WHERE id =?`, [id], (err, res)=>{
@@ -22,44 +15,44 @@ exports.getUser = (id, cb)=>{
     });
 };
 
-exports.checkContact = (contact, cb)=>{
-    db.query(`SELECT * FROM ${table} WHERE email=? OR phone_number=?`, contact, (err, res)=>{
-        if(err) throw err;
-        cb(res);
+exports.getUsername = (username)=>new Promise((resolve, reject)=>{
+    db.query(`SELECT * FROM ${table} WHERE username='${username}'`, (err, res)=>{
+        if(err) reject(err);
+        resolve(res);
     });
-};
+});
 
-exports.checkEmail = (email, cb)=>{
-    db.query(`SELECT * FROM ${table} WHERE email=?`, [email], (err, res)=>{
-        if(err) throw err;
-        cb(res);
+exports.getEmail = (email)=>new Promise((resolve, reject)=>{
+    db.query(`SELECT * FROM ${table} WHERE email='${email}'`, (err, res)=>{
+        if(err) reject(err);
+        resolve(res);
     });
-};
+});
 
-exports.checkPhone = (phone, cb)=>{
-    db.query(`SELECT * FROM ${table} WHERE phone_number=?`, [phone], (err, res)=>{
-        if(err) throw err;
-        cb(res);
+exports.getPhone = (phone_number)=>new Promise((resolve, reject)=>{
+    db.query(`SELECT * FROM ${table} WHERE phone_number=${phone_number}`, (err, res)=>{
+        if(err) reject(err);
+        resolve(res);
     });
-};
+});
 
-exports.addUser = (data, cb)=>{
-    db.query(`INSERT INTO ${table} (name, email, password, phone_number, gender, birthdate, address) VALUES(?,?,?,?,?,?,?)`, data, (err, res)=>{
-        if(err) throw err;
-        cb(res);
+exports.addUser = async (data) => new Promise ((resolve, reject) => {
+    db.query(`INSERT INTO ${table} (name, email, username, password, phone_number, gender, birthdate, address) VALUES ('${data.name}', '${data.email}', '${data.username}', '${data.password}', '${data.phone_number}', '${data.gender}', '${data.birthdate}', '${data.address}')`, (err, res) => {
+        if(err) reject(err);
+        resolve(res);
     });
-};
+});
 
-exports.updateUser = (data, cb)=>{
-    db.query(`UPDATE ${table} SET name=?, email=?, password=?, phone_number=?, gender=?, birthdate=?, address=? WHERE id=?`, data, (err,res)=>{
-        if(err) throw err;
-        cb(res);
+exports.updateUser = (data, id)=>new Promise((resolve, reject)=>{
+    db.query(`UPDATE ${table} SET ? WHERE id=?`, [data, id], (err, res)=>{
+        if(err) reject(err);
+        resolve(res);
     });
-};
+});
 
-exports.deleteUser = (id, cb)=>{
-    db.query(`DELETE FROM ${table} WHERE id=?`, [id], (err,res)=>{
-        if(err) throw err;
-        cb(res);
+exports.deleteUser = (id)=>new Promise((resolve, reject)=>{
+    db.query(`DELETE FROM ${table} WHERE id=?`, [id], (err, res)=>{
+        if(err) reject(err);
+        resolve(res);
     });
-};
+});
