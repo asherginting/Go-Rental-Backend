bde26348c77138e7cadbcb332e4ab32c6b633083
Asher Azriel Ginting <asherginting3@gmail.com>
2022-02-28 14:32:05 +0700

    feat: add cors

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const express = require('express');
 require('dotenv').config();
+const multer = require('multer');
+const cors = require('cors');
 
+const upload = multer();
 const app = express();
 
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(require('./src/routes'));
+
+// app.use('/uploads', express.static('uploads'));
+
+app.use(upload.array());
 app.use('/uploads', express.static('uploads'));
 
-const {PORT, APP_PORT} = process.env;
+const { PORT, APP_PORT } = process.env;
 
 app.listen(PORT || APP_PORT, () => {
-    console.log(`App Listening on port ${PORT || APP_PORT}`);
+  // eslint-disable-next-line no-console
+  console.log(`App running on port ${PORT || APP_PORT}`);
 });
