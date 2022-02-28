292eb89fef835858dff40ab58d1a73fb3a576f93
Asher Azriel Ginting <asherginting3@gmail.com>
2022-02-28 14:44:11 +0700

    fix: index js

--- index.js
+++ index.js
@@ -11,14 +11,12 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(require('./src/routes'));
 
-// app.use('/uploads', express.static('uploads'));
-
 app.use(upload.array());
 app.use('/uploads', express.static('uploads'));
 
 const { PORT, APP_PORT } = process.env;
 
 app.listen(PORT || APP_PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`App running on port ${PORT || APP_PORT}`);
+    // eslint-disable-next-line no-console
+    console.log(`App running on port ${PORT || APP_PORT}`);
 });
