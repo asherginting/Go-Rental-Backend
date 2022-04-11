dcdb7b00c6ccce0615abd7c5bd6c5a8325b3b3f3
asherginting <asherginting3@gmail.com>
2022-04-11 23:00:57 +0700

    fix: configure db

--- src/helpers/db.js
+++ src/helpers/db.js
@@ -1,19 +1,62 @@
+// const mysql = require('mysql');
+
+// const {
+//     DB_HOST, DB_USER, DB_PASSWORD, DB_NAME,
+// } = process.env;
+
+// const conn = mysql.createConnection({
+//     host: DB_HOST,
+//     user: DB_USER,
+//     password: DB_PASSWORD,
+//     database: DB_NAME,
+// });
+
+// conn.connect((err) => {
+//     if (err) throw err;
+//     console.log('connection to database is successful');
+// });
+
+// module.exports = conn;
+
 const mysql = require('mysql');
+require('dotenv').config();
 
 const {
-    DB_HOST, DB_USER, DB_PASSWORD, DB_NAME,
+    DB_HOST,
+    DB_USER,
+    DB_PASSWORD,
+    DB_NAME
 } = process.env;
 
-const conn = mysql.createConnection({
+const config = {
     host: DB_HOST,
     user: DB_USER,
     password: DB_PASSWORD,
-    database: DB_NAME,
-});
+    database: DB_NAME
+};
 
-conn.connect((err) => {
-    if (err) throw err;
-    console.log('connection to database is successful');
-});
+let connection;
 
-module.exports = conn;
+const handleDisconnect = () => {
+    connection = mysql.createConnection(config);
+    connection.connect((err) => {
+        if (err) {
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000);
+        }
+    });
+
+    connection.on('error', (err) => {
+        console.log('db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            handleDisconnect();
+        } else {
+            throw err;
+        }
+    });
+};
+
+handleDisconnect();
+console.log('db connected');
+
+module.exports = connection;
