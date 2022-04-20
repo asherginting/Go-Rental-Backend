b0745791428511e8195aa281b279d6fad1758f08
asherginting <asherginting3@gmail.com>
2022-04-20 14:30:26 +0700

    fix: db add pool

--- src/helpers/db.js
+++ src/helpers/db.js
@@ -11,46 +11,4 @@ const conn = mysql.createPool({
     database: DB_NAME,
 });
 
-// conn.connect();
-
 module.exports = conn;
-
-// const {
-//   DB_HOST,
-//   DB_USER,
-//   DB_PASSWORD,
-//   DB_NAME,
-// } = process.env;
-
-// const config = {
-//   host: DB_HOST,
-//   user: DB_USER,
-//   password: DB_PASSWORD,
-//   database: DB_NAME,
-// };
-
-// let connection;
-
-// const handleDisconnect = () => {
-//   connection = mysql.createConnection(config);
-//   connection.connect((err) => {
-//     if (err) {
-//       console.log('error when connecting to db:', err);
-//       setTimeout(handleDisconnect, 2000);
-//     }
-//   });
-
-//   connection.on('error', (err) => {
-//     console.log('db error', err);
-//     if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-//       handleDisconnect();
-//     } else {
-//       throw err;
-//     }
-//   });
-// };
-
-// handleDisconnect();
-// console.log('db connected');
-
-// module.exports = connection
